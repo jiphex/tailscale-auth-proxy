425f3a9e17a94f78423d5bae51c8c5fb37707aab
James Hannah <git@james.jph.im>
2025-09-14 20:52:54 +0100

    refactor systemd stuff for multi proxies
    this is never gonna work lol

--- flake.nix
+++ flake.nix
@@ -2,44 +2,80 @@
   description = "A very basic flake";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils } :
-  flake-utils.lib.eachDefaultSystem (system: 
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.default = pkgs.buildGoModule {
-        src = ./.;
-        name = "tailscale-auth-proxy";
-        vendorHash = "sha256-1hhztYJcTduwkm99cElsA9tp7hra8Tf8bQzPlh9zSvA";
-    };
-    apps.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/cmd";
-    };
-    nixosModules.tailscaleAuthProxy = { config, lib, ...}: {
-      options.services.tailscaleAuthProxy = with lib; {
-        enable = lib.mkEnableOption "enable tailscale auth proxy";
-        upstream = lib.mkOption {
-          type = types.str;
-          default = "http://localhost:3000";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          src = ./.;
+          name = "tailscale-auth-proxy";
+          vendorHash = "sha256-1hhztYJcTduwkm99cElsA9tp7hra8Tf8bQzPlh9zSvA";
         };
-        listenAddr = lib.mkOption {
-          type = types.str;
-          default = ":38388";
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/cmd";
         };
-      };
-      config = lib.mkIf config.services.tailscaleAuthProxy.enable {
-        systemd.services.tailscale-auth-proxy = {
-          wantedBy = [ "multi-user.target" ];
-          serviceConfig = {
-            User = "tailscaleap";
-            Group = "tailscaleap";
-            DynamicUser = true;
-            Restart = "always";
-            ExecStart = "${self.packages."${system}".default}/bin/cmd";
+        nixosModules.tailscaleAuthProxy = { config, lib, ... }: {
+          options.services.tailscaleAuthProxy = with lib; {
+            enable = lib.mkEnableOption "enable tailscale auth proxy";
+            proxies = lib.mkOption {
+              description = "List of upstream/downstream proxy configs";
+              type = lib.types.attrsOf
+                (lib.types.submodule
+                  ({ config, ... }: {
+                    options = {
+                      bind = lib.mkOption
+                        {
+                          description = "bind address";
+                          type = lib.types.str;
+                        };
+                      app = lib.mkOption
+                        {
+                          description = "app address";
+                          type = lib.types.str;
+                        };
+                    };
+                  }));
+            };
+            default = {
+              grafana = {
+                bind = ":38388";
+                app = "localhost:3000";
+              };
+            };
           };
+          config =
+            lib.mkIf
+              config.services.tailscaleAuthProxy.enable
+              {
+                # systemd.services.tailscale-auth-proxy = {
+                #   wantedBy = [ "multi-user.target" ];
+                #   serviceConfig = {
+                #     User = "tailscaleap";
+                #     Group = "tailscaleap";
+                #     DynamicUser = true;
+                #     Restart = "always";
+                #     ExecStart = "${self.packages."${system}".default}/bin/cmd";
+                #   };
+                # };
+                systemd.services = lib.attrsets.mapAttrs'
+                  (name: value: {
+                    name = "tailscale-auth-proxy@${name}";
+                    value = {
+                      wantedBy = [ "multi-user.target" ];
+                      serviceConfig = {
+                        User = "tailscaleap";
+                        Group = "tailscaleap";
+                        DynamicUser = true;
+                        Restart = "always";
+                        ExecStart = "${self.packages."${system}".default}/bin/cmd -listen ${value.bind} -upstream ${value.app}";
+                      };
+                    };
+                  })
+                  config.services.tailscaleAuthProxy.proxies;
+              };
         };
-      };
-    };
-  });
+      });
 }
--- go.mod
+++ go.mod
@@ -1,21 +1,19 @@
 module github.com/jiphex/ts-id
 
-go 1.25.1
+go 1.23
 
-require tailscale.com v1.88.1
+require tailscale.com v1.75.0-pre
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/akutz/memconn v0.1.0 // indirect
 	github.com/alexbrainman/sspi v0.0.0-20231016080023-1a75b4708caa // indirect
 	github.com/coder/websocket v1.8.14 // indirect
 	github.com/dblohm7/wingoes v0.0.0-20250822163801-6d8e6105c62d // indirect
 	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
-	github.com/go-json-experiment/json v0.0.0-20250910080747-cc2cfa0554c3 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250103232110-6a9a0fde9288 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/hdevalence/ed25519consensus v0.2.0 // indirect
-	github.com/josharian/native v1.1.1-0.20230202152459-5c7d0dd6ab86 // indirect
 	github.com/jsimonetti/rtnetlink v1.4.2 // indirect
 	github.com/mdlayher/netlink v1.8.0 // indirect
 	github.com/mdlayher/socket v0.5.1 // indirect
@@ -24,13 +22,11 @@ require (
 	github.com/x448/float16 v0.8.4 // indirect
 	go4.org/mem v0.0.0-20240501181205-ae6ca9944745 // indirect
 	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
-	golang.org/x/crypto v0.42.0 // indirect
-	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
-	golang.org/x/mod v0.28.0 // indirect
-	golang.org/x/net v0.44.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/sys v0.36.0 // indirect
-	golang.org/x/text v0.29.0 // indirect
-	golang.org/x/tools v0.37.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/exp v0.0.0-20250210185358-939b2ce775ac // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
 	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
 )
